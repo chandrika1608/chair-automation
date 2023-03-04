2dc3902d0836d3025339eca658fe8e1eae7a4141
Muhil kannan <muhil2801@gmail.com>
2023-03-04 17:02:28 +0530

    Combo implemented done

---
+++ app/src/main/java/com/lixo/pos/repository/ComboRepository.java
@@ -0,0 +1,14 @@
+package com.lixo.pos.repository;
+import com.lixo.pos.model.Combo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface ComboRepository extends JpaRepository<Combo,Long> {
+    List<Combo> findAllByRestaurantId(Long restaurantId);
+
+    Optional<Combo> findByRestaurantIdAndId(Long restaurantId, Long id);
+
+}
